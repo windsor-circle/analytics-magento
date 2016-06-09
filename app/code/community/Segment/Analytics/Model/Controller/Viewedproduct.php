0e0274c694efe300a2b1db7dea37f86abdf0b673
Caleb Vandenheuvel <caleb.vandenheuvel@windsorcircle.com>
2016-06-09 14:28:01 -0400

    Revert "Viewedproduct.php"
    This reverts commit dab1f2dccae3a60e4d962f252893dfe58d5bc30d.

--- app/code/community/Segment/Analytics/Model/Controller/Viewedproduct.php
+++ app/code/community/Segment/Analytics/Model/Controller/Viewedproduct.php
@@ -7,7 +7,7 @@ class Segment_Analytics_Model_Controller_Viewedproduct extends Segment_Analytics
         // $product = Mage::getModel('catalog/product_api')->info($params['id']);
 
         if (!array_key_exists('id', $params) || is_null($params['id'])) {
-            return $block;
+            return;
         }
         $product   = Mage::helper('segment_analytics')
         ->getNormalizedProductInformation($params['id']);   
